09c9d2bed68b34efc63f23b9ff32484dbac912b0
SVI-NET\Md_Kittipong <kittipong.chan09@gmail.com>
2025-10-01 13:34:57 +0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import viteLogo from '/200.webp'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>I'm Brueoat !!!</h1>
+    </>
+  )
+}
+
+export default App
